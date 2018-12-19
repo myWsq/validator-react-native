135d46a99e9a10cf0658f3d9bdc7470d238830c9
wsq <mywsq961@gmail.com>
2018-12-19 22:28:46 +0800

    新增touch_id

--- android/app/src/main/java/com/validator/MainApplication.java
+++ android/app/src/main/java/com/validator/MainApplication.java
@@ -3,6 +3,7 @@ package com.validator;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnfingerprint.FingerprintAuthPackage;
 import com.react.rnspinkit.RNSpinkitPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FingerprintAuthPackage(),
             new RNSpinkitPackage(),
             new ImagePickerPackage(),
             new RNGestureHandlerPackage(),

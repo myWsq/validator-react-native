d419df7d011e909225a3bf3d6afd0f7eef2467b8
wsq <mywsq961@gmail.com>
2019-01-02 22:47:34 +0800

    增加TouchID

--- screens/MainStack/Home/Record/index.tsx
+++ screens/MainStack/Home/Record/index.tsx
@@ -9,40 +9,46 @@ import TouchID from 'react-native-touch-id';
 
 export interface RecordScreenProps extends NavigationScreenProps {}
 
-export interface RecordScreenState {}
+export interface RecordScreenState {
+	isAuth: boolean;
+}
 
 export default class RecordScreen extends React.Component<RecordScreenProps, RecordScreenState> {
 	constructor(props: RecordScreenProps) {
 		super(props);
 
-		this.state = {};
+		this.state = {
+			isAuth: false,
+		};
 	}
 
 	public render() {
 		return (
 			<Container style={{}}>
-				<ScrollView style={{ paddingLeft: 20, paddingRight: 20 }}>
-					<MyTitle>出示身份二维码</MyTitle>
-					<MyText>时间: 2018/8/21 上午 9:40:50 </MyText>
-					<Divider />
-					<MyTitle>来自 12306 的授权请求</MyTitle>
-					<MyText>原因: 购买 G83 北京西 9∶00 长沙南 14∶40 二等座车票 </MyText>
-					<MyText>时间: 2018/9/18 下午 5:13:45 </MyText>
-					<Divider />
-					<MyTitle>出示身份二维码</MyTitle>
-					<MyText>时间: 2018/9/20 上午 10:50:18 </MyText>
-					<Divider />
-					<MyTitle>来自 12306 的授权请求</MyTitle>
-					<MyText>原因: 购买 G86 北京西 14∶00 长沙南 21∶30 二等座车票 </MyText>
-					<MyText>时间: 2018/10/1 下午 6:20:57 </MyText>
-					<Divider />
-					<MyTitle>出示身份二维码</MyTitle>
-					<MyText>时间: 2018/10/2 下午 1:35:40 </MyText>
-					<Divider />
-					<Text style={{ color: color.info, textAlign: 'center', marginTop: 30, marginBottom: 30 }}>
-						没有更多记录了
-					</Text>
-				</ScrollView>
+				{this.state.isAuth && (
+					<ScrollView style={{ paddingLeft: 20, paddingRight: 20 }}>
+						<MyTitle>出示身份二维码</MyTitle>
+						<MyText>时间: 2018/8/21 上午 9:40:50 </MyText>
+						<Divider />
+						<MyTitle>来自 12306 的授权请求</MyTitle>
+						<MyText>原因: 购买 G83 北京西 9∶00 长沙南 14∶40 二等座车票 </MyText>
+						<MyText>时间: 2018/9/18 下午 5:13:45 </MyText>
+						<Divider />
+						<MyTitle>出示身份二维码</MyTitle>
+						<MyText>时间: 2018/9/20 上午 10:50:18 </MyText>
+						<Divider />
+						<MyTitle>来自 12306 的授权请求</MyTitle>
+						<MyText>原因: 购买 G86 北京西 14∶00 长沙南 21∶30 二等座车票 </MyText>
+						<MyText>时间: 2018/10/1 下午 6:20:57 </MyText>
+						<Divider />
+						<MyTitle>出示身份二维码</MyTitle>
+						<MyText>时间: 2018/10/2 下午 1:35:40 </MyText>
+						<Divider />
+						<Text style={{ color: color.info, textAlign: 'center', marginTop: 30, marginBottom: 30 }}>
+							没有更多记录了
+						</Text>
+					</ScrollView>
+				)}
 			</Container>
 		);
 	}
@@ -56,10 +62,14 @@ export default class RecordScreen extends React.Component<RecordScreenProps, Rec
 				fallbackTitle: '',
 			});
 			if (!isAuthenticate) {
-				this.props.navigation.goBack()
+				this.props.navigation.goBack();
+			} else {
+				this.setState({
+					isAuth: true,
+				});
 			}
 		} catch (error) {
-			this.props.navigation.goBack()
+			this.props.navigation.goBack();
 		}
 	}
 }
